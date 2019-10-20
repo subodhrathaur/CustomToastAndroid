c8b231b8832a720e9f70e5cb9f8c046febac24aa
Subodh Kumar <rathaurshivomsingh@gmail.com>
2019-10-20 08:50:29 +0530

    first commit

---
+++ app/src/main/java/com/subodh/customtoast/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.subodh.customtoast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun showToast(view: View) {
+
+    }
+}
