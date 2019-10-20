32f8b4a18bd1919fbaebe5d929ad2e07d65ad158
Subodh Kumar <rathaurshivomsingh@gmail.com>
2019-10-20 10:02:47 +0530

    still in development

--- app/src/main/java/com/subodh/customtoast/MainActivity.kt
+++ app/src/main/java/com/subodh/customtoast/MainActivity.kt
@@ -3,6 +3,7 @@ package com.subodh.customtoast
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +12,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun showToast(view: View) {
-
+    fun successtoast(view: View) {
+    CustomToast.makeText(applicationContext,"Success",Toast.LENGTH_SHORT,CustomToast.SUCCESS).show()
+    }
+    fun warningtoast(view: View) {
+        CustomToast.makeText(applicationContext,"Warning",Toast.LENGTH_SHORT,CustomToast.WARNING).show()
+    }
+    fun errortoast(view: View) {
+        CustomToast.makeText(applicationContext,"Error",Toast.LENGTH_SHORT,CustomToast.ERROR).show()
+    }
+    fun infotoast(view: View) {
+        CustomToast.makeText(applicationContext,"Info",Toast.LENGTH_SHORT,CustomToast.INFO).show()
+    }
+    fun defaulttoast(view: View) {
+        CustomToast.makeText(applicationContext,"Defaullt",Toast.LENGTH_SHORT,CustomToast.DEFAULT).show()
+    }
+    fun confusingtoast(view: View) {
+        CustomToast.makeText(applicationContext,"Confusing",Toast.LENGTH_SHORT,CustomToast.CONFUSING).show()
     }
 }
---
+++ subodhtoast/src/main/java/com/subodh/customtoast/CustomToast.kt
@@ -0,0 +1,107 @@
+package com.subodh.customtoast
+
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.graphics.Color
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+
+import com.android.subodhtoast.R
+
+class CustomToast(context: Context) : Toast(context) {
+    companion object {
+
+        var SUCCESS = 1
+        var WARNING = 2
+        var ERROR = 3
+        var INFO = 4
+        var DEFAULT = 5
+        var CONFUSING = 6
+
+        fun makeText(
+            context: Context,
+            message: String,
+            duration: Int,
+            type: Int
+        ): Toast {
+            val toast = Toast(context)
+            toast.duration = duration
+            val layout = LayoutInflater.from(context).inflate(R.layout.customtoast, null, false)
+            val l1 = layout.findViewById<View>(R.id.toast_text) as TextView
+            val linearLayout = layout.findViewById<View>(R.id.toast_type) as LinearLayout
+            val img = layout.findViewById<View>(R.id.toast_icon) as ImageView
+            l1.text = message
+            when (type) {
+                1 -> {
+                    linearLayout.setBackgroundResource(R.drawable.success_toast_shape)
+                    img.setImageResource(R.drawable.tick)
+                }
+                2 -> {
+                    linearLayout.setBackgroundResource(R.drawable.warning_toast_shape)
+                    img.setImageResource(R.drawable.danger)
+                }
+                3 -> {
+                    linearLayout.setBackgroundResource(R.drawable.error_toast_shape)
+                    img.setImageResource(R.drawable.close)
+                }
+                4 -> {
+                    linearLayout.setBackgroundResource(R.drawable.info_toast_shape)
+                    img.setImageResource(R.drawable.information)
+                }
+                5 -> {
+                    linearLayout.setBackgroundResource(R.drawable.default_shape)
+                    img.visibility = View.GONE
+                }
+                6 -> {
+                    linearLayout.setBackgroundResource(R.drawable.confusing_toast_shape)
+                    img.setImageResource(R.drawable.confused)
+                }
+            }
+            toast.view = layout
+            return toast
+        }
+
+
+        fun makeText(
+            context: Context,
+            message: String,
+            duration: Int,
+            type: Int,
+            ImageResource: Int,
+            androidIcon: Boolean
+        ): Toast {
+            val toast = Toast(context)
+            val layout = LayoutInflater.from(context).inflate(R.layout.customtoast, null, false)
+            val l1 = layout.findViewById<View>(R.id.toast_text) as TextView
+            val linearLayout = layout.findViewById<View>(R.id.toast_type) as LinearLayout
+            val img = layout.findViewById<View>(R.id.toast_icon) as ImageView
+            l1.text = message
+            img.setImageResource(ImageResource)
+            when (type) {
+                1 -> linearLayout.setBackgroundResource(R.drawable.success_toast_shape)
+                2 -> linearLayout.setBackgroundResource(R.drawable.warning_toast_shape)
+                3 -> linearLayout.setBackgroundResource(R.drawable.error_toast_shape)
+                4 -> linearLayout.setBackgroundResource(R.drawable.info_toast_shape)
+                5 -> {
+                    linearLayout.setBackgroundResource(R.drawable.default_shape)
+                    img.visibility = View.GONE
+                }
+                6 -> linearLayout.setBackgroundResource(R.drawable.confusing_toast_shape)
+                else -> {
+                    linearLayout.setBackgroundResource(R.drawable.default_shape)
+                    img.visibility = View.GONE
+                }
+            }
+            toast.view = layout
+            return toast
+        }
+    }
+
+}
